4c5119b0729b003df34032889744657a2a282204
dobermensch <dobermensch@gmail.com>
2024-06-20 16:19:38 +0800

    add: comment in config for enableLogs

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ dotenv.config()
 const config = {
   btcBetAmount: process.env.BTC_BET_AMOUNT || '0.01',
   claimAfterRounds: parseInt(process.env.CLAIM_AFTER_ROUNDS || '1'),
-  enableLogs: !!process.env.ENABLE_LOGS,
+  enableLogs: !!process.env.ENABLE_LOGS, // leave empty to disable logging
   ethBetAmount: process.env.ETH_BET_AMOUNT || '0.01',
   numOfRounds: parseInt(process.env.NUM_OF_ROUNDS || '1'),
 }
